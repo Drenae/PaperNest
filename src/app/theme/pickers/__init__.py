23b6c398698424520fefa569fabaffcb1fc00be6
Drenae <adrien.guilpain@gmail.com>
2026-08-01 05:34:04 +0200

    refactor(theme): add pickers package

---
+++ src/app/theme/pickers/__init__.py
@@ -0,0 +1,13 @@
+from .color_picker import BaseColorPicker
+from .date_picker import BaseDatePickerField
+from .file_picker import BaseFilePicker, FilePickerErrorHandler
+from .icon_picker import BaseIconPicker, PaperNestIconPickerOption
+
+__all__ = [
+    "BaseColorPicker",
+    "BaseDatePickerField",
+    "BaseFilePicker",
+    "BaseIconPicker",
+    "FilePickerErrorHandler",
+    "PaperNestIconPickerOption",
+]
